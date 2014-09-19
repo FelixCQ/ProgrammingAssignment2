9036db6f0e44fb934e569c405549ea80e92e5b97
Félix de Chaumont Quitry <felix.chq@gmail.com>
2014-09-18 22:29:22 -0400

    Complete assignment 2 from https://class.coursera.org/rprog-007

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file defines a cachematrix object, encapsulates a matrix and caches
+## its inverse.
 
-## Write a short comment describing this function
+## This create a new cachematrix object.
+## The object is implemented as a list of functions.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(mat = matrix()) {
+        inv <- NULL
+        set <- function(newmat) {
+                mat <<- newmat
+                inv <<- NULL
+        }
+        get <- function() mat
+        setinverse <- function(i) inv <<- i
+        getinverse <- function() inv
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function takes a cachematrix as first argument, and acts like the
+## usual solve() function, except the solve operation is only performed once.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(cachemat, ...) {
+        inv <- cachemat$getinverse()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        actualmat <- cachemat$get()
+        inv <- solve(actualmat, ...)
+        cacheat$setinverse(inv)
+        inv
 }
